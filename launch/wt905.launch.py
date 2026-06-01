e82d12f48c6b0cb2ddc57931c4495ce50f2bea1e
christopher-blum <90465568+christopher-blum@users.noreply.github.com>
2026-06-01 08:24:40 +0100

    Add restart function to IMU wt905.launch.py

--- launch/wt905.launch.py
+++ launch/wt905.launch.py
@@ -17,7 +17,9 @@ def generate_launch_description():
         name='witmotion_ros_node',
         package = 'witmotion_ros',
         executable = 'witmotion_ros_node',
-        parameters = [config]
+        parameters = [config],
+        respawn=True,
+        respawn_delay=2.0
     )
 
     ld.add_action(node)
