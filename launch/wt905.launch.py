09740f972d902b6d310015553ac5bbd83f517a16
christopher-blum <90465568+christopher-blum@users.noreply.github.com>
2026-06-01 11:14:06 +0100

    Reinsert node name into wt905.launch.py

--- launch/wt905.launch.py
+++ launch/wt905.launch.py
@@ -14,6 +14,7 @@ def generate_launch_description():
         )
         
     node=Node(
+        name = 'witmotion_ros_node',
         package = 'witmotion_ros',
         executable = 'witmotion_ros_node',
         parameters = [config],
