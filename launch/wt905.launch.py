db06162e991b2e67461944777e16984091feb4e8
christopher-blum <90465568+christopher-blum@users.noreply.github.com>
2026-06-01 10:43:42 +0100

    Revert wt905.launch.py in case that's what's breaking everything

--- launch/wt905.launch.py
+++ launch/wt905.launch.py
@@ -14,12 +14,9 @@ def generate_launch_description():
         )
         
     node=Node(
-        name='witmotion_ros_node',
         package = 'witmotion_ros',
         executable = 'witmotion_ros_node',
         parameters = [config],
-        # respawn=True,
-        # respawn_delay=2.0
     )
 
     ld.add_action(node)
