b2f77334809b9ce3943a663fb86625e4b6c81100
christopher-blum <90465568+christopher-blum@users.noreply.github.com>
2026-06-02 11:12:03 +0100

    Update wt905.launch.py to add restart to imu node

--- launch/wt905.launch.py
+++ launch/wt905.launch.py
@@ -18,6 +18,8 @@ def generate_launch_description():
         package = 'witmotion_ros',
         executable = 'witmotion_ros_node',
         parameters = [config],
+        respawn=True,
+        respawn_delay=5.0
     )
 
     ld.add_action(node)
